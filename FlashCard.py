28a86c791204059782e5c4a88f8103cd188a23e2
amosyu2000 <46848538+amosyu2000@users.noreply.github.com>
2019-03-05 15:50:37 -0500

    Release v0.1

---
+++ FlashCard.py
@@ -0,0 +1,48 @@
+'''
+The FlashCard class creates cards.
+
+Created on Feb 23, 2019
+@author: amosyu2000
+'''
+
+from tkinter import *
+import tkinter.ttk as ttk
+import random
+from PIL import ImageTk, Image
+import os
+
+class FlashCard:
+    
+    skinList = os.listdir("CardSkins")[0:10]
+    
+    skinValueList = []
+    for file in skinList:
+        skinValueList.append(int(file[:2]))
+    
+    def createSkin(self, image):
+        colourImage = Image.open("CardSkins/"+image)
+        greyImage = Image.open("CardSkins/"+image)
+        greyMask = Image.open("CardSkins/greyMask.png")
+        greyImage.paste(greyMask, mask=greyMask)
+        
+        return ImageTk.PhotoImage(colourImage), ImageTk.PhotoImage(greyImage)
+    
+    def setGreyScale(self, boolean=True):
+        if boolean:
+            self.label.config(image=self.greySkin)
+        else:
+            self.label.config(image=self.skin)
+            
+    def destroy(self):
+        self.frame.destroy()
+    
+    def __init__(self, master):
+        randomIndex = random.randint(1,len(self.skinList)) - 1
+        self.value = self.skinValueList[randomIndex]
+        self.skin, self.greySkin = self.createSkin(self.skinList[randomIndex])
+        
+        self.frame = Frame(master)
+        self.frame.pack(side=LEFT)
+        
+        self.label = Label(self.frame, image=self.skin)
+        self.label.pack(padx=10)
---
+++ run.py
@@ -0,0 +1,6 @@
+from tkinter import Tk
+from Main import TwentyFourGame
+
+root = Tk()
+run = TwentyFourGame(root)
+root.mainloop()
