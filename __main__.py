b1aeaf2fc03068e4c6125fb23abb49b5aaf95177
Amos Yu <amosyu2000@gmail.com>
2019-12-02 20:03:20 -0500

    better project structure

---
+++ __main__.py
@@ -0,0 +1,7 @@
+from tkinter import Tk
+from Main import TwentyFourGame
+
+if __name__ == '__main__':
+	root = Tk()
+	run = TwentyFourGame(root)
+	root.mainloop()
--- run.py
+++
@@ -1,6 +0,0 @@
-from tkinter import Tk
-from Main import TwentyFourGame
-
-root = Tk()
-run = TwentyFourGame(root)
-root.mainloop()
